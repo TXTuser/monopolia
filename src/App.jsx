8315fae11ca9de5230ee736b00e68174f4be62c1
TXTuser <gimosteg666@gmail.com>
2024-04-06 16:00:29 +0300

    #diceError

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,61 @@
 import map from "./img/map.svg";
 import cat from "./img/blackcat.png";
+import dice1 from "./img/dice1.png";
+import dice2 from "./img/dice2.png";
+import dice3 from "./img/dice3.png";
+import dice4 from "./img/dice4.png";
+import dice5 from "./img/dice5.png";
+import dice6 from "./img/dice6.png";
 import "./App.css";
 // @ts-ignore
 import { cardsDB, usersDB } from "./db";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [cards, setCards] = useState(cardsDB);
   const [users, setUsers] = useState(usersDB);
-  const [steps,setSteps] = useState(5)
+  const [steps, setSteps] = useState(40);
+  const [stepsRight, setStepsRight] = useState(11);
+  const [stepsBottom, setStepsBottom] = useState(0);
+  const [numDice, setNumDice] = useState(10)
+  const [dices, setDices] = useState([eval("dice" + Math.floor(Math.random() * 6 + 1)),eval("dice" + Math.floor(Math.random() * 6 + 1))])
+  const [diceImages, setDiceImages] = useState([dice1,dice2,dice3,dice4,dice5,dice6])
+  useEffect(() => {
+    getRight();
+    getBottom();
+    diceRoll();
+  }, []);
+  function diceRoll() {
+    let random = Math.floor(Math.random() * 6 + 1);
+    let random2 = Math.floor(Math.random() * 6 + 1);
+  }
+
+  function getRight() {
+    let right = stepsRight;
+    if (steps < 11) {
+      right = 11 + 8.5 * (steps - 1);
+    } else if (steps < 21) {
+      right = 87.5;
+    } else if (steps < 31) {
+      right = 11 + 8.5 * (9 - (steps - 20));
+    } else if (steps < 41) {
+      right = 0;
+    }
+    setStepsRight(right);
+  }
+  function getBottom() {
+    let bottom = stepsBottom;
+    if (steps < 11) {
+      bottom = 0;
+    } else if (steps < 21) {
+      bottom = 11 + 8.5 * (steps - 11);
+    } else if (steps < 31) {
+      bottom = 87.5;
+    } else if (steps < 41) {
+      bottom = 11 + 8.5 * (8 - (steps - 31));
+    }
+    setStepsBottom(bottom);
+  }
   return (
     <div className="App">
       <div className="game-info">
@@ -68,200 +115,23 @@ function App() {
               </nav>
             </li>
           ))}
-          {/* <li>
-            <a href="" className="player-name">barbie</a>
-            <nav className="game-info-nav">
-              <div className="card">
-                <span className="color-card"></span>
-                <div className="card-info">
-                  <h2 className="city">Ул. Арбат</h2>
-                  <h4>Рента с участка</h4>
-                  <ul className="monopoly-rent">
-                    <li>
-                      <span>Рента при Монополии</span>
-                      <span>100</span>
-                    </li>
-                    <li>
-                      <span>С 1 домом</span>
-                      <span>200</span>
-                    </li>
-                    <li>
-                      <span>С 2 домами</span>
-                      <span>600</span>
-                    </li>
-                    <li>
-                      <span>С 3 домами</span>
-                      <span>1400</span>
-                    </li>
-                    <li>
-                      <span>С 4 домами</span>
-                      <span>1700</span>
-                    </li>
-                    <li>
-                      <span>С Отелем</span>
-                      <span>2000</span>
-                    </li>
-                  </ul>
-                  <ul className="priceforhome">
-                    <li>
-                      <span>Стоимость дома</span>
-                      <span>200</span>
-                    </li>
-                    <li>
-                      <span>Стоимость отеля</span>
-                      <span>200</span>
-                    </li>
-                  </ul>
-                  <ul></ul>
-                </div>
-              </div>
-            </nav>
-          </li>
-          <li>
-            <a href="" className="player-name">barbie</a>
-            <nav className="game-info-nav">
-              <div className="card">
-                <span className="color-card"></span>
-                <div className="card-info">
-                  <h2 className="city">adadasdasd Ул.</h2>
-                  <h4>Рента с участка</h4>
-                  <ul>
-                    <li>
-                      <span>Рента при Монополии</span>
-                      <span>100</span>
-                    </li>
-                    <li>
-                      <span>С 1 домом</span>
-                      <span>200</span>
-                    </li>
-                    <li>
-                      <span>С 2 домами</span>
-                      <span>600</span>
-                    </li>
-                    <li>
-                      <span>С 3 домами</span>
-                      <span>1400</span>
-                    </li>
-                    <li>
-                      <span>С 4 домами</span>
-                      <span>1700</span>
-                    </li>
-                    <li>
-                      <span>С Отелем</span>
-                      <span>2000</span>
-                    </li>
-                  </ul>
-                  <ul className="priceforhome">
-                    <li>
-                      <span>Стоимость дома</span>
-                      <span>200</span>
-                    </li>
-                    <li>
-                      <span>Стоимость отеля</span>
-                      <span>200</span>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-
-              <div className="card">
-                <span className="color-card"></span>
-                <div className="card-info">
-                  <h2 className="city">Нагатинская Ул.</h2>
-                  <h4>Рента с участка</h4>
-                  <ul>
-                    <li>
-                      <span>Рента при Монополии</span>
-                      <span>100</span>
-                    </li>
-                    <li>
-                      <span>С 1 домом</span>
-                      <span>200</span>
-                    </li>
-                    <li>
-                      <span>С 2 домами</span>
-                      <span>600</span>
-                    </li>
-                    <li>
-                      <span>С 3 домами</span>
-                      <span>1400</span>
-                    </li>
-                    <li>
-                      <span>С 4 домами</span>
-                      <span>1700</span>
-                    </li>
-                    <li>
-                      <span>С Отелем</span>
-                      <span>2000</span>
-                    </li>
-                  </ul>
-                  <ul className="priceforhome">
-                    <li>
-                      <span>Стоимость дома</span>
-                      <span>200</span>
-                    </li>
-                    <li>
-                      <span>Стоимость отеля</span>
-                      <span>200</span>
-                    </li>
-                  </ul>
-                  <ul></ul>
-                </div>
-              </div>
-
-              <div className="card">
-                <span className="color-card"></span>
-                <div className="card-info">
-                  <h2 className="city">Житная Ул.</h2>
-                  <h4>Рента с участка</h4>
-                  <ul>
-                    <li>
-                      <span>Рента при Монополии</span>
-                      <span>100</span>
-                    </li>
-                    <li>
-                      <span>С 1 домом</span>
-                      <span>200</span>
-                    </li>
-                    <li>
-                      <span>С 2 домами</span>
-                      <span>600</span>
-                    </li>
-                    <li>
-                      <span>С 3 домами</span>
-                      <span>1400</span>
-                    </li>
-                    <li>
-                      <span>С 4 домами</span>
-                      <span>1700</span>
-                    </li>
-                    <li>
-                      <span>С Отелем</span>
-                      <span>2000</span>
-                    </li>
-                  </ul>
-                  <ul className="priceforhome">
-                    <li>
-                      <span>Стоимость дома</span>
-                      <span>200</span>
-                    </li>
-                    <li>
-                      <span>Стоимость отеля</span>
-                      <span>200</span>
-                    </li>
-                  </ul>
-                  <ul></ul>
-                </div>
-              </div>
-            </nav>
-          </li> */}
         </ol>
       </div>
       <div className="container">
+        <div id="dices" onClick={() => diceRoll()}>
+          <img src={diceImages[0]} alt="" />
+          <img src={diceImages[1]} alt="" />
+        </div>
         <img src={map} alt="" className="map" />
-        <img src={cat} alt="" className="cat" style={{
-          right:"%", /*11 - с большой на маленький///8.5 - с маленькой на маленькую */
-        }}/>
+        <img
+          src={cat}
+          alt=""
+          className="cat"
+          style={{
+            right: stepsRight + "%",
+            bottom: stepsBottom + "%",
+          }}
+        />
       </div>
     </div>
   );
